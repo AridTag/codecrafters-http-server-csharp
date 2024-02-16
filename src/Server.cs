2f42e49ffe81ad397a6c802c25f77502010ed95a
AridTag <AridTag@binarydepths.com>
2024-02-16 04:41:07 -0500

    Stage 2

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,35 @@
+using System.Buffers;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
+var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket();
+
+var buffer = new Memory<byte>(new byte[1024], 0, 1024);
+while (true)
+{
+    using var client = await server.AcceptTcpClientAsync();
+
+    try
+    {
+        NetworkStream clientStream = client.GetStream();
+        var bytesRead = await clientStream.ReadAsync(buffer);
+        var request = Encoding.ASCII.GetString(buffer.Span[..bytesRead]);
+        var response = Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+        await clientStream.WriteAsync(response);
+    }
+    catch (SocketException ex)
+    {
+        if (ex.SocketErrorCode == SocketError.ConnectionReset)
+        {
+            Console.WriteLine("Client severed connection");
+        }
+        else
+        {
+            Console.WriteLine(ex.SocketErrorCode);
+        }
+
+        break;
+    }
+}
